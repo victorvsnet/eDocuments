e113c8acbfb4fecf11f2ad24c13eb247cd482df9
victorvsnet <victorvsnet@gmail.com>
2017-02-25 18:48:13 -0500

    DASeguimiento & DASolicitud

--- eDocuments.DataAccess/DASeguimientoSolicitud.cs
+++ eDocuments.DataAccess/DASeguimientoSolicitud.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using eDocuments.Common;
+using eDocuments.Entities;
+using Npgsql;
+using NpgsqlTypes;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +13,41 @@ namespace eDocuments.DataAccess
 {
     public class DASeguimientoSolicitud
     {
+        public int RegistrarSeguimientoSolicitud(BESeguimientoSolicitud oParametro)
+        {
+            int idRegistro = 0;
+            try
+            {
+                using (NpgsqlConnection ocn = new NpgsqlConnection(Util.getConnection()))
+                {
+                    ocn.Open();
+                    NpgsqlTransaction tran = ocn.BeginTransaction();
+                    using (NpgsqlCommand ocmd = new NpgsqlCommand("public.func_registrar_doc_seg_solicitud", ocn))
+                    {
+                        ocmd.CommandType = CommandType.StoredProcedure;
+                        ocmd.Parameters.Add("@p_cod_solicitud", NpgsqlDbType.Varchar).Value = oParametro.cod_solicitud;
+                        ocmd.Parameters.Add("@p_cod_estado", NpgsqlDbType.Integer).Value = oParametro.cod_estado;
+                        ocmd.Parameters.Add("@p_gls_observacion", NpgsqlDbType.Varchar).Value = oParametro.gls_observacion;
+                        ocmd.Parameters.Add("@p_aud_usr_ingreso", NpgsqlDbType.Varchar).Value = oParametro.aud_usr_ingreso;
+
+                        using (NpgsqlDataReader odr = ocmd.ExecuteReader())
+                        {
+                            while (odr.Read())
+                            {
+                                idRegistro = Convert.ToInt32(odr[0]);
+                            }
+                        }
+
+                    }
+                    tran.Commit();
+                    ocn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return idRegistro;
+        }
     }
 }
