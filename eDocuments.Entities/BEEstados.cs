b977cf280b029039fd40753a256a07faf1932e85
victorvsnet <victorvsnet@gmail.com>
2017-02-25 19:19:12 -0500

    Estados de la Solicitud

---
+++ eDocuments.Entities/BEEstados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eDocuments.Entities
+{
+    public class BEEstados : Auditoria
+    {
+        public int cod_estado { get; set; }
+        public string gls_estado { get; set; }
+    }
+}
