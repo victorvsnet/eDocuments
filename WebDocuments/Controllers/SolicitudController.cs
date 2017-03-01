5fd25ef99c5bfce1647ec49e10f7161160509ce8
ASUS <ASUS@DESKTOP-Q3RJIIC>
2017-03-01 00:59:10 -0500

    cambios 01/03/2017 12:58am

---
+++ WebDocuments/Controllers/SolicitudController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebDocuments.Controllers
+{
+    public class SolicitudController : Controller
+    {
+        // GET: Solicitud
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/Documento"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "Documento cargado correctamente!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Falló carga de documento!";
+                return View();
+            }
+        }
+    }
+}
---
+++ WebDocuments/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDocuments.Models
+{
+    public class UserModel
+    {
+        public string cod_usuario { get; set; }
+
+        public string pass_usuario { get; set; }
+    }
+}
